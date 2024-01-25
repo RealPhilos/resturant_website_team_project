e9d90972c8f228d00645b6b6877a406ef1224ac7
zlac157 <zlac157@live.rhul.ac.uk>
2024-01-25 19:59:00 +0000

    Implement ClassPathResource to access LoginDatabase.txt

--- Restaurant/src/main/java/restaurant/Login.java
+++ Restaurant/src/main/java/restaurant/Login.java
@@ -1,4 +1,4 @@
-package restaurant;
+/*package restaurant;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -30,4 +30,47 @@ public class Login { private File loginDatabaseFile = new File("/home/krish/git/
     return password.equals(loginDatabase.get(username));
   }
   
+}
+*/
+
+package restaurant;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Login {
+    private HashMap<String, String> loginDatabase;
+
+    // Converts the database file in a HashMap.
+    private HashMap<String, String> fileReader() throws IOException {
+        HashMap<String, String> login = new HashMap<>();
+
+        // Accessing the file using Spring Boot's ClassPathResource
+        Resource resource = new ClassPathResource("Database/LoginDatabase.txt");
+        InputStream inputStream = resource.getInputStream();
+        Scanner reader = new Scanner(inputStream);
+
+        while (reader.hasNextLine()) {
+            String[] line = reader.nextLine().split(" ");
+            String username = line[0];
+            String password = line[1];
+            login.put(username, password);
+        }
+        reader.close();
+
+        return login;
+    }
+
+    public Login() throws IOException {
+        this.loginDatabase = fileReader();
+    }
+
+    public boolean checkLogin(String username, String password) {
+        return password.equals(loginDatabase.get(username));
+    }
 }
