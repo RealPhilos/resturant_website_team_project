2a4c47b87a9dc5402ffc171599eea8533d162587
ahmedalyami <ahmadalyami2016@gmail.com>
2024-02-01 19:07:17 +0000

    Restructured the routes for both customer and waiter login

---
+++ frontend/src/app/customer/signup/page.jsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import React from "react";
+
+function CustomerSignUpPage() {
+  return (
+    <div>
+      <form className="mx-auto w-2/5 border border-blue-500 rounded p-8 mt-12 flex flex-col gap-7 items-center">
+        <span className="text-xl font-bold">Customer Sign Up</span>
+        <div className="flex flex-col w-3/4 gap-1">
+          <label>First Name</label>
+          <input
+            className="p-2 bg-gray-200 rounded-md"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="flex flex-col w-3/4 gap-1">
+          <label>Last Name</label>
+          <input
+            className="p-2 bg-gray-200 rounded-md"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="flex flex-col w-3/4 gap-1">
+          <label>Email</label>
+          <input
+            className="p-2 bg-gray-200 rounded-md"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="flex flex-col w-3/4 gap-1">
+          <label>Password</label>
+          <input
+            className="p-2 bg-gray-200 rounded-md"
+            placeholder="Enter your password"
+          />
+        </div>
+        <div className="flex flex-col w-3/4 gap-1">
+          <label>Confirm Password</label>
+          <input
+            className="p-2 bg-gray-200 rounded-md"
+            placeholder="Enter your password"
+          />
+        </div>
+        <input
+          className="bg-blue-500 p-3 w-40 rounded-lg text-white cursor-pointer"
+          type="submit"
+          value="Sign Up"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default CustomerSignUpPage;
