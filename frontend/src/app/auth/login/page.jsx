1fcfbb83877fecda7e5d99d079f34ca8ddb087fe
KUMAR Parvesh (2022) WLIS205 <wlis205@live.rhul.ac.uk>
2024-02-01 08:57:08 +0000

    removed signup route as it is not needed

---
+++ frontend/src/app/auth/login/page.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div>
+      <form className='mx-auto w-2/5 border border-indigo-600 rounded p-8 mt-12 flex flex-col gap-7 items-center'>
+        <span className='text-xl font-bold'>Waiter Login</span>
+        <div className='flex flex-col w-3/4 gap-1'>
+          <label>Email</label>
+          <input
+            className='p-2 bg-gray-200 rounded-md'
+            placeholder='Enter your email'
+          />
+        </div>
+        <div className='flex flex-col w-3/4 gap-1'>
+          <label>Password</label>
+          <input
+            className='p-2 bg-gray-200 rounded-md'
+            placeholder='Enter your password'
+          />
+        </div>
+        <input
+          className='bg-indigo-500 p-3 w-40 rounded-lg text-white cursor-pointer'
+          type='submit'
+        />
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ frontend/src/app/menu/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MenuPage() {
+  return (
+    <div>
+      <h2>Delicious Menus</h2>
+    </div>
+  );
+}
+
+export default MenuPage;
