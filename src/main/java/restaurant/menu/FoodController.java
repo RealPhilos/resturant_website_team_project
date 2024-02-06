e4c701a6e68e4c7169c644872de42cb1051ddc2b
Zkac229 <zkac229@live.rhul.ac.uk>
2024-02-06 01:20:15 +0000

    Update updateOrder to include additional parameters

--- src/main/java/restaurant/menu/FoodController.java
+++ src/main/java/restaurant/menu/FoodController.java
@@ -40,8 +40,12 @@ public class FoodController {
 
   @PutMapping(path = "{foodId}")
   public void updateOrder(@PathVariable("foodId") Long foodId,
-      @RequestParam(required = true) int qty, @RequestParam(required = true) String food) {
-    foodService.updateFood(foodId, qty, food);
+      @RequestParam(required = true) int qty, 
+      @RequestParam(required = true) String food, 
+      @RequestParam(required = false) String description, 
+      @RequestParam(required = false) String category, 
+      @RequestParam(required = false) Double price){
+    foodService.updateFood(foodId, qty, food, description, category, price);
 
   }
 
