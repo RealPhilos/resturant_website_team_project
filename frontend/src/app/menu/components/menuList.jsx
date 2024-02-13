919ea30a16e0c865566c5d675b256d208069585d
ahmedalyami <ahmadalyami2016@gmail.com>
2024-02-13 21:59:07 +0000

    removed unnecessary packages from the project

--- frontend/src/app/menu/components/menuList.jsx
+++ frontend/src/app/menu/components/menuList.jsx
@@ -18,39 +18,6 @@ function MenuList() {
 
     fetchMenus();
   }, []);
-  // const menus = [
-  //   {
-  //     name: "Pizza",
-  //     description: "Ingredients - Cheese, Tomato, Chicken, Onion",
-  //     price: 12.99,
-  //   },
-  //   {
-  //     name: "Burger",
-  //     description: "Ingredients - Cheese, Tomato, Chicken, Onion",
-  //     price: 11.99,
-  //   },
-  //   {
-  //     name: "Sandwich",
-  //     description: "Ingredients - Cheese, Tomato, Chicken, Onion",
-  //     price: 10.99,
-  //   },
-  //   {
-  //     name: "Pasta",
-  //     description: "Ingredients - Cheese, Tomato, Chicken, Onion",
-  //     price: 11.49,
-  //     image: "/pasta.jpeg",
-  //   },
-  //   {
-  //     name: "Burger",
-  //     description: "Ingredients - Cheese, Tomato, Chicken, Onion",
-  //     price: 12.99,
-  //   },
-  //   {
-  //     name: "Sandwich",
-  //     description: "Ingredients - Cheese, Tomato, Chicken, Onion",
-  //     price: 12.99,
-  //   },
-  // ];
 
   return (
     <div className="grid grid-cols-3 gap-4 gap-x-12 mt-8">
