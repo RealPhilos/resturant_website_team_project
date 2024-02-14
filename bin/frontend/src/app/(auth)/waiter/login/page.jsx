143d5582e040169a32f3df9ff3a792fcaf4f63be
zjac003 <zjac003@live.rhul.ac.uk>
2024-02-14 19:03:08 +0000

    add figma file link showcase front end pages integrated with backend

---
+++ bin/frontend/src/app/(auth)/waiter/login/page.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div>
+      <form className="mx-auto w-2/5 border border-green-800 rounded p-8 mt-12 flex flex-col gap-7 items-center">
+        <span className="text-xl font-bold">Waiter Login</span>
+        <div className="flex flex-col w-3/4 gap-1">
+          <label>Email</label>
+          <input
+            className="p-2 bg-gray-200 rounded-md"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="flex flex-col w-3/4 gap-1">
+          <label>Password</label>
+          <input
+            className="p-2 bg-gray-200 rounded-md"
+            placeholder="Enter your password"
+          />
+          <span className="text-sm text-gray-600">
+            Already a member? Sign Up{" "}
+            <Link className="text-green-900 font-semibold" href="/auth/signup">
+              here!
+            </Link>
+          </span>
+        </div>
+        <input
+          className="bg-green-800 p-3 w-40 rounded-lg text-white cursor-pointer"
+          type="submit"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
