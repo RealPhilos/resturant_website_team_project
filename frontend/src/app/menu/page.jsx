a98624bc76bee552d9cb364720c4674f0f9429d9
zlac157 <zlac157@live.rhul.ac.uk>
2024-02-15 22:13:03 +0000

    Changed import files location

--- frontend/src/app/menu/page.jsx
+++ frontend/src/app/menu/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import MenuList from "./menuList";
-import MenuHeader from "./menuHeader";
+import MenuList from "./components/menuList";
+import MenuHeader from "./components/menuHeader";
 
 function MenuPage() {
   return (
