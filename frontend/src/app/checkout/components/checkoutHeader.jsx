72f87634e0d1bf07931eead18529c24965cfa101
KUMAR Parvesh (2022) WLIS205 <wlis205@live.rhul.ac.uk>
2024-02-15 19:16:54 +0000

    added link in navbar for checkout ui

--- frontend/src/app/checkout/components/checkoutHeader.jsx
+++ frontend/src/app/checkout/components/checkoutHeader.jsx
@@ -1,7 +1,7 @@
 function CheckoutHeader() {
   return (
     <div className="flex py-5 items-end gap-10">
-      <span className="text-5xl font-serif">Basket</span>
+      <span className="text-5xl font-serif">Order</span>
       <span className="text-md text-gray-600">3 items</span>
     </div>
   );
