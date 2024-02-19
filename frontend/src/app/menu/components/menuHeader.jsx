fd19f485da944b756a0d4af67c653f38732a6d97
Zkac229 <zkac229@live.rhul.ac.uk>
2024-02-19 16:28:49 +0000

    feat(MenuHeader): Made filter buttons on menu header change colour when clicked.

--- frontend/src/app/menu/components/menuHeader.jsx
+++ frontend/src/app/menu/components/menuHeader.jsx
@@ -1,6 +1,27 @@
-import React from "react";
 
+"use client";
+
+import React, { useState } from "react";
+
+const FilterButtons = ["Default", "Price low to high", "Price high to low"];
+
+/**
+ * Functional component that represents the header of the menu.
+ *
+ * @returns The rendered header component.
+ */
 function MenuHeader() {
+  const [activeButton, setActiveButton] = useState("Default");
+
+  /**
+   * Handles the button click event and updates the active button state.
+   *
+   * @param string buttonName - The name of the clicked button.
+   */
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
   return (
     <div className="py-6 flex justify-between items-end">
       <div className="flex items-end gap-10">
@@ -8,19 +29,22 @@ function MenuHeader() {
         <span>All items served fresh with fresh ingredients</span>
       </div>
       <div>
-        <button className="py-2 bg-green-700 text-white mr-2 px-3 text-sm rounded-3xl">
-          Default
-        </button>
-        <button className="py-2 border-gray-500 border text-black mr-2 px-4 text-sm rounded-3xl">
-          A-Z
-        </button>
-        <button className="py-2 border-gray-500 border text-black text-sm px-3 rounded-3xl">
-          List view
-        </button>
+        {FilterButtons.map((buttonName) => (
+          <button
+            key={buttonName}
+            className={
+              buttonName === activeButton
+                ? "py-2 bg-green-700 text-white mr-2 px-3 text-sm rounded-3xl"
+                : "py-2 border-gray-500 border text-black mr-2 px-4 text-sm rounded-3xl"
+            }
+            onClick={() => handleButtonClick(buttonName)}
+          >
+            {buttonName}
+          </button>
+        ))}
       </div>
     </div>
   );
 }
-
 
 export default MenuHeader;
