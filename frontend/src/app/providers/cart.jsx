561d258e1b9ea686338cd7a0a007833aa94f9f2d
KUMAR Parvesh (2022) WLIS205 <wlis205@live.rhul.ac.uk>
2024-02-23 14:03:20 +0000

    added addItem function in cartState

--- frontend/src/app/providers/cart.jsx
+++ frontend/src/app/providers/cart.jsx
@@ -1,8 +1,10 @@
 "use client";
+
 import { createContext, useState } from "react";
 
 export const CartContext = createContext({
   items: [],
+  addItem: () => {},
 });
 
 export default function CartContextProvider({ children }) {
@@ -26,7 +28,23 @@ export default function CartContextProvider({ children }) {
       quantity: 2,
     },
   ]);
+
+  function addItem(i) {
+    if (items.find((item) => item.id == i.id)) {
+      setItems(
+        items.map((item) => {
+          if (item.id == id) {
+            item.quantity++;
+          }
+          return item;
+        })
+      );
+    }
+    setItems([...items, i]);
+  }
   return (
-    <CartContext.Provider value={{ items }}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ items, addItem }}>
+      {children}
+    </CartContext.Provider>
   );
 }
