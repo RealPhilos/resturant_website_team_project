7f2b7342bb0d35fc0e5506400da024db92298e0c
ahmedalyami <ahmadalyami2016@gmail.com>
2024-02-27 01:07:20 +0000

    adjusted the image

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,67 +1,25 @@
 "use client";
 
-import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      {/* <Head>
-        <title>Oaxaca Restaurant</title>
-        <meta name="description" content="This is the Oaxaca Restaurant Menu" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-        <link href="/styles/tailwind.css" rel="stylesheet" />
-      </Head>
-
-      <main>
-        <div>
-          <nav className="flex justify-center flex-wrap">
-            <Link href="/">
-              <button className="px-5 py-2 text-lg font-bold text-black bg-blue-300 m-1 transition-colors duration-300 hover:bg-white">
-                Home
-              </button>
-            </Link>
-            <Link href="/menu">
-              <button className="px-5 py-2 text-lg font-bold text-black bg-blue-300 m-1 transition-colors duration-300 hover:bg-white">
-                Menu
-              </button>
-            </Link>
-            <Link href="/order">
-              <button className="px-5 py-2 text-lg font-bold text-black bg-blue-300 m-1 transition-colors duration-300 hover:bg-white">
-                Order
-              </button>
-            </Link>
-            <Link href="/track">
-              <button className="px-5 py-2 text-lg font-bold text-black bg-blue-300 m-1 transition-colors duration-300 hover:bg-white">
-                Track
-              </button>
-            </Link>
-            <Link href="/bill">
-              <button className="px-5 py-2 text-lg font-bold text-black bg-blue-300 m-1 transition-colors duration-300 hover:bg-white">
-                Billing
-              </button>
-            </Link>
-            <Link href="/feedback">
-              <button className="px-5 py-2 text-lg font-bold text-black bg-blue-300 m-1 transition-colors duration-300 hover:bg-white">
-                Feedback
-              </button>
-            </Link>
-            <Link href="/customer/login">
-              <button className="px-5 py-2 text-lg font-bold text-black bg-blue-300 m-1 transition-colors duration-300 hover:bg-white">
-                Login
-              </button>
-            </Link>
-          </nav>
-        </div>
-        <h1>Welcome To Oaxaca Restaurant!</h1>
-        The welcome page for the restaurant.
-        <br></br>
-      </main>
-
-      <footer>Video of Restaurant, Reviews</footer> */}
+    <div className="flex flex-col items-center w-full">
+      <span className="text-5xl font-serif text-gray-700 mt-24">
+        Organic, Fresh foods.
+      </span>
+      <span className="text-lg text-gray-500">Since 1901.</span>
+      <Link
+        className="bg-green-700 text-white px-4 py-2 rounded-lg mt-16 mb-12"
+        href="/menu"
+      >
+        Browse our menu
+      </Link>
+      <img className="h-2/3 w-5/6" src="pizza1.jpg" alt="" />
+      <span className="text-2xl text-gray-600 mt-8 mb-32 px-32 text-center">
+        Serving authentic original recipes with fresh meat and veg to people all
+        over the UK for over 100 years
+      </span>
     </div>
   );
 }
