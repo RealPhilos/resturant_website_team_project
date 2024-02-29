5853fe0d5e3fec9b69de4fb15212f7bbf89c70e6
ahmedalyami <ahmadalyami2016@gmail.com>
2024-02-29 09:54:03 +0000

    added states for the required fields in the signup page

--- frontend/src/app/(auth)/customer/signup/page.jsx
+++ frontend/src/app/(auth)/customer/signup/page.jsx
@@ -1,7 +1,24 @@
-import Link from "next/link";
-import React from "react";
+"use client";
+
+import { useState } from "react";
 
 function CustomerSignUpPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
   return (
     <div>
       <form className="mx-auto w-2/5 border border-green-800 rounded p-8 mt-12 flex flex-col gap-7 items-center">
@@ -9,6 +26,8 @@ function CustomerSignUpPage() {
         <div className="flex flex-col w-3/4 gap-1">
           <label>Username</label>
           <input
+            value={username}
+            onChange={handleUsernameChange}
             className="p-2 bg-gray-200 rounded-md"
             placeholder="Enter your email"
           />
@@ -16,6 +35,8 @@ function CustomerSignUpPage() {
         <div className="flex flex-col w-3/4 gap-1">
           <label>Password</label>
           <input
+            value={password}
+            onChange={handlePasswordChange}
             className="p-2 bg-gray-200 rounded-md"
             placeholder="Enter your password"
           />
@@ -23,6 +44,8 @@ function CustomerSignUpPage() {
         <div className="flex flex-col w-3/4 gap-1">
           <label>Confirm Password</label>
           <input
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
             className="p-2 bg-gray-200 rounded-md"
             placeholder="Enter your password"
           />
