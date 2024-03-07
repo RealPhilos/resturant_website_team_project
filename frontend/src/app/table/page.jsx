b6e69e729809f64efe9213a01bd1f1adb8154c6f
zjac003 <zjac003@live.rhul.ac.uk>
2024-03-07 19:38:47 +0000

    feat(table): add footer from main page for consistency

--- frontend/src/app/table/page.jsx
+++ frontend/src/app/table/page.jsx
@@ -2,6 +2,7 @@
 import TableColour from "../components/table-colour";
 import { useState } from 'react';
 import Footer from '../components/footer';
+import Link from "next/link";
 
 const TablePage = () => {
 
@@ -106,15 +107,88 @@ const TablePage = () => {
         >Reserve</button>
 
         <div className="min-h-screen">
-        {/* Your application content */}
-        {/* For example: */}
+        
         <header>
-          {/* Header content */}
+          
         </header>
         <main>
-          {/* Main content */}
+          
         </main>
-        <Footer />
+
+        <br></br>
+      <hr />
+      <br></br>
+      <br></br>
+
+
+
+
+        <footer>
+        <div
+          className="footer"
+          style={{ display: "flex", justifyContent: "left" }}
+        >
+          <div style={{ marginRight: "5em", textAlign: "center" }}>
+            <h1 className="text-l font-serif mb-1">Other Information</h1>
+            <ul>
+              <li className="text-xs">
+                <Link href="/">About Us</Link>
+              </li>
+              <li className="text-xs">
+                <Link href="/">Careers</Link>
+              </li>
+              <li className="text-xs">
+                <Link href="/">More Oaxaca</Link>
+              </li>
+              <li className="text-xs">
+                <Link href="/">Help</Link>
+              </li>
+            </ul>
+          </div>
+          <div style={{ marginRight: "5em", textAlign: "center" }}>
+            <h1 className="text-l font-serif mb-1">Follow Us!</h1>
+            <ul>
+              <li className="text-xs">
+                <Link href="/">Instagram</Link>
+              </li>
+              <li className="text-xs">
+                <Link href="/">Twitter</Link>
+              </li>
+              <li className="text-xs">
+                <Link href="/">Facebook</Link>
+              </li>
+            </ul>
+          </div>
+          <div style={{ marginRight: "20em", textAlign: "center" }}>
+            <h1 className="text-l font-serif mb-1">Download Our App!</h1>
+            <ul>
+              <li className="text-xs">
+                <Link href="/">App Store</Link>
+              </li>
+              <li className="text-xs">
+                <Link href="/">Google Play Store</Link>
+              </li>
+            </ul>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <span className="text-xl text-green-800">Oaxaca Restaurant</span>
+          </div>
+        </div>
+
+        <br></br>
+        <hr />
+
+        <div className="text-xs" style={{ textAlign: "right" }}>
+          <Link href="/">Privacy Statement |</Link>
+          <Link href="/"> Terms & Conditions |</Link>
+          <Link href="/"> Cookie Policy </Link>
+          {/* Add other links here */}
+        </div>
+
+        <p className="text-xs" style={{ textAlign: "right" }}>
+          © 2023 – 2024 Oaxaca’s. All Rights Reserved
+        </p>
+      </footer>
       </div>
     </div>
   );  
