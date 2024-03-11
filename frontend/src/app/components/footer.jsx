54253fe80682e72dde473e7b2d63215fe0e19eaf
LUQUE_VAN_DER_MERWE <zlac180@live.rhul.ac.uk>
2024-03-11 12:29:19 +0000

    Fix(footer): updated and refactored footer and extracted to footer.jsx file

--- frontend/src/app/components/footer.jsx
+++ frontend/src/app/components/footer.jsx
@@ -1,17 +1,77 @@
 // Footer.jsx
 
 import React from 'react';
+import Link from "next/link";
+
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-200 py-4 mt-8">
-      <div className="container mx-auto px-4">
-      <div className="flex justify-between items-center">
-          {/* Content of the footer */}
-          <p className="text-gray-600">&copy; 2023 – 2024 Oaxaca’s. All Rights Reserved</p>
-        </div>
+    <footer>
+    <div
+      className="footer"
+      style={{ display: "flex", justifyContent: "left" }}
+    >
+      <div style={{ marginRight: "5em", textAlign: "center" }}>
+        <h1 className="text-l font-serif mb-1">Other Information</h1>
+        <ul>
+          <li className="text-xs">
+            <Link href="/">About Us</Link>
+          </li>
+          <li className="text-xs">
+            <Link href="/">Careers</Link>
+          </li>
+          <li className="text-xs">
+            <Link href="/">More Oaxaca</Link>
+          </li>
+          <li className="text-xs">
+            <Link href="/">Help</Link>
+          </li>
+        </ul>
       </div>
-    </footer>
+      <div style={{ marginRight: "5em", textAlign: "center" }}>
+        <h1 className="text-l font-serif mb-1">Follow Us!</h1>
+        <ul>
+          <li className="text-xs">
+            <Link href="/">Instagram</Link>
+          </li>
+          <li className="text-xs">
+            <Link href="/">Twitter</Link>
+          </li>
+          <li className="text-xs">
+            <Link href="/">Facebook</Link>
+          </li>
+        </ul>
+      </div>
+      <div style={{ marginRight: "20em", textAlign: "center" }}>
+        <h1 className="text-l font-serif mb-1">Download Our App!</h1>
+        <ul>
+          <li className="text-xs">
+            <Link href="/">App Store</Link>
+          </li>
+          <li className="text-xs">
+            <Link href="/">Google Play Store</Link>
+          </li>
+        </ul>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <span className="text-xl text-green-800">Oaxaca Restaurant</span>
+      </div>
+    </div>
+
+    <br></br>
+    <hr />
+
+    <div className="text-xs" style={{ textAlign: "right" }}>
+      <Link href="/">Privacy Statement |</Link>
+      <Link href="/"> Terms & Conditions |</Link>
+      <Link href="/"> Cookie Policy </Link>
+      {/* Add other links here */}
+    </div>
+
+    <p className="text-xs" style={{ textAlign: "right" }}>
+      © 2023 – 2024 Oaxaca’s. All Rights Reserved
+    </p>
+  </footer>
   );
 };
 
