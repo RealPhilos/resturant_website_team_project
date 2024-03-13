3dd961b7f47d34810bc1ae5f729e81bb65d51112
zlac157 <zlac157@live.rhul.ac.uk>
2024-03-13 15:37:22 +0000

    Fix: no need to instantiate a User object.

--- backend/src/main/java/restaurant/order/ItemConfig.java
+++ backend/src/main/java/restaurant/order/ItemConfig.java
@@ -5,8 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import restaurant.customer.User;
-import restaurant.customer.UserRepository;
+
 
 /**
  * Configuration class for initializing items and associating them with users.
@@ -26,20 +25,15 @@ public class ItemConfig {
    * @return A CommandLineRunner for initializing items and user associations.
    */
   @Bean
-  CommandLineRunner itemDataInitializer(ItemRepository itemRepository,
-      UserRepository userRepository) {
+  CommandLineRunner itemDataInitializer(ItemRepository itemRepository) {
     return args -> {
-      User user1 =
-          userRepository.getUser("Krish").orElseThrow(() -> new RuntimeException("User not found"));
-      User user2 = userRepository.getUser("Malcolm")
-          .orElseThrow(() -> new RuntimeException("User not found"));
-      Item sushi = new Item("sushi", 3, "table 1", user1);
+      Item sushi = new Item("sushi", 3, "table 1", "Malcolm");
       sushi.setStatus("ordered");
-      Item pasta = new Item("pasta", 2, "table 1", user1);
+      Item pasta = new Item("pasta", 2, "table 1", "Malcolm");
       pasta.setStatus("cooking");
-      Item pizza = new Item("pizza", 3, "table 2", user2);
+      Item pizza = new Item("pizza", 3, "table 2", "Krish");
       pizza.setStatus("done");
-      Item burger = new Item("burger", 3, "table 2", user2);
+      Item burger = new Item("burger", 3, "table 2", "Krish");
       burger.setStatus("delivered");
       itemRepository.saveAll(List.of(sushi, pasta, pizza, burger));
     };
