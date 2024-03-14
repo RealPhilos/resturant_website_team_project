de486464e4967d7d0fba37c0a3c95c6f8518b8de
ahmedalyami <ahmadalyami2016@gmail.com>
2024-03-14 00:11:29 +0000

    implemented tabs for order page

--- frontend/src/app/(admin)/admin-orders/page.jsx
+++ frontend/src/app/(admin)/admin-orders/page.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function OrderPage() {
-  return <div>OrderPage</div>;
+  return (
+    <>
+      <Tabs defaultValue="all">
+        <TabsList className="mb-3 flex w-[30%] justify-between">
+          <TabsTrigger className="cursor-pointer" value="all">
+            <p>All</p>
+          </TabsTrigger>
+          <TabsTrigger className="cursor-pointer" value="processing">
+            <p>Processing</p>
+          </TabsTrigger>
+          <TabsTrigger className="cursor-pointer" value="ready">
+            <p>Ready</p>
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="all">
+          <p>All</p>
+        </TabsContent>
+        <TabsContent value="processing">
+          <p>Processing</p>
+        </TabsContent>
+        <TabsContent value="ready">
+          <p>Ready</p>
+        </TabsContent>
+      </Tabs>
+    </>
+  );
 }
