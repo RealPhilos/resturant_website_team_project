5116165211e9a58060277c85fbc5e4dc059e23c4
KUMAR Parvesh (2022) WLIS205 <wlis205@live.rhul.ac.uk>
2024-03-14 19:04:16 +0000

    implemented menu fetching in waiter menu page

--- frontend/src/app/(admin)/admin-menu/page.jsx
+++ frontend/src/app/(admin)/admin-menu/page.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+"use client";
+
+import api from "@/app/services/api";
+import { useEffect, useState } from "react";
 
 export default function MenuPage() {
-  return <div>MenuPage</div>;
+  const [menus, setMenus] = useState([]);
+
+  useEffect(() => {
+    const fetchMenu = async () => {
+      try {
+        const response = await api.get("/foods");
+        // Sort the menu items alphabetically by name by default
+        setMenus(response.data.sort((a, b) => a.name.localeCompare(b.name)));
+      } catch (error) {
+        console.error("Error fetching menu data:", error);
+      }
+    };
+
+    fetchMenu();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
 }
