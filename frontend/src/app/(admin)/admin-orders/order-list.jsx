38a021b41c15f8205780d5caeb015e9a2b30a74b
Zkac229 <zkac229@live.rhul.ac.uk>
2024-03-15 07:32:00 +0000

    feat(OrderTracking: Kitchen Staff can now see orders from customers

--- frontend/src/app/(admin)/admin-orders/order-list.jsx
+++ frontend/src/app/(admin)/admin-orders/order-list.jsx
@@ -7,26 +7,35 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-export default function OrderList() {
+
+export default function OrderList({ order }) {
+  console.log(order);
+  if (!order) {
+    return null; 
+  }
+
+  const orderDate = new Date(order.orderTime[0], order.orderTime[1] - 1, order.orderTime[2], order.orderTime[3], order.orderTime[4], order.orderTime[5]);
+  const formattedOrderTime = orderDate.toLocaleTimeString();
+
   return (
     <Table>
       <TableHeader>
         <TableRow>
           <TableHead className="w-[100px]">Id</TableHead>
-          <TableHead>Menus</TableHead>
+          <TableHead>Item</TableHead>
           <TableHead>Customer</TableHead>
-          <TableHead>Amount</TableHead>
+          <TableHead>Order Time</TableHead>
           <TableHead className="text-right">Status</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         <TableRow>
           <TableCell className="font-medium">1</TableCell>
-          <TableCell>1 x Pizza, 2 x Burger</TableCell>
-          <TableCell>Ahmed Alyami</TableCell>
-          <TableCell>£25.00</TableCell>
+          <TableCell>{order.quantity} x {order.name}</TableCell>
+          <TableCell>{order.username}</TableCell>
+          <TableCell>{formattedOrderTime}</TableCell>
           <TableCell className="text-right text-green-600">
-            Processing
+            {order.status}
           </TableCell>
         </TableRow>
       </TableBody>
--- frontend/src/app/(admin)/admin-orders/page.jsx
+++ frontend/src/app/(admin)/admin-orders/page.jsx
@@ -1,11 +1,34 @@
+"use client";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import OrderList from "./order-list";
+import api from "../../services/api";
+
+import React, { useEffect, useState } from "react";
 
 export default function OrderPage() {
+
+  const [orders, setOrders] = useState([]);
+
+  
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await api.get("/order");
+        // Sort the menu items alphabetically by name by default
+       
+
+        setOrders(response.data);
+      } catch (error) {
+        console.error("Error fetching order data:", error);
+      }
+    };
+    fetchOrders();
+  }, []);
+
   return (
     <div className="py-10 px-6">
       <span className="text-4xl font-semibold">
-        Orders <span className="text-green-800">(20)</span>
+        Orders <span className="text-green-800">({orders.length})</span>
       </span>
       <Tabs className="mt-5" defaultValue="all">
         <TabsList className="mb-3 flex w-[30%] justify-between">
@@ -20,6 +43,9 @@ export default function OrderPage() {
           </TabsTrigger>
         </TabsList>
         <TabsContent value="all">
+        {orders.map((order) => (
+          <OrderList key={order.id} order={order} />
+          ))}
           <OrderList />
         </TabsContent>
         <TabsContent value="processing">
