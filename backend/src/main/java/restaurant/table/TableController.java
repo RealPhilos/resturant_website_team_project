25773c9b59397a433d00fd862217e04507b3d1de
KUMAR Parvesh (2022) WLIS205 <wlis205@live.rhul.ac.uk>
2024-03-22 01:30:37 +0000

    fixed checkstyle violations in Item and ItemRepository files

--- backend/src/main/java/restaurant/table/TableController.java
+++ backend/src/main/java/restaurant/table/TableController.java
@@ -10,45 +10,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * Controller class for table management.
+ * 
  * @author Kasim
  */
 @RestController
 @RequestMapping(path = "Table")
 public class TableController {
-	private TableService tableService;
+  private TableService tableService;
 
-	@Autowired
-	public TableController(TableService tableService) {
-		this.tableService = tableService;
-	}
+  @Autowired
+  public TableController(TableService tableService) {
+    this.tableService = tableService;
+  }
 
-	@GetMapping
-	public List<TableReservation> getAllTable() {
-		return tableService.getAllTable();
-	}
+  @GetMapping
+  public List<TableReservation> getAllTable() {
+    return tableService.getAllTable();
+  }
 
-	@PostMapping(path = "/bookTable/{username}/{size}")
-	public boolean bookTable(@PathVariable("username") String username, @PathVariable("size") int size) {
-		return tableService.bookTable(username, size);
-	}
+  @PostMapping(path = "/bookTable/{username}/{size}")
+  public boolean bookTable(@PathVariable("username") String username,
+      @PathVariable("size") int size) {
+    return tableService.bookTable(username, size);
+  }
 
-	@PostMapping(path = "/tableFinish/{username}")
-	public void tableFinish(@PathVariable("username") String username) {
-		tableService.tableFinish(username);
-	}
+  @PostMapping(path = "/tableFinish/{username}")
+  public void tableFinish(@PathVariable("username") String username) {
+    tableService.tableFinish(username);
+  }
 
-	@PostMapping(path = "/tableCleaned/{tableNumber}")
-	public void tableCleaned(@PathVariable("tableNumber") int tableNumber) {
-		tableService.tableCleaned(tableNumber);
-	}
+  @PostMapping(path = "/tableCleaned/{tableNumber}")
+  public void tableCleaned(@PathVariable("tableNumber") int tableNumber) {
+    tableService.tableCleaned(tableNumber);
+  }
 
-	/**
-	 * 
-	 * @return list of TableReservations.
-	 */
-	@GetMapping("/all") // Makes a Get request to /Table/all retrieving all table reservations from
-						// database.
-	public List<TableReservation> getAllTableReservations() {
-		return tableService.getAllTableReservations();
-	}
+  /**
+   * Method for getting all table reservations.
+   * 
+   * @return list of TableReservations.
+   */
+  @GetMapping("/all") // Makes a Get request to /Table/all retrieving all table reservations from
+  public List<TableReservation> getAllTableReservations() {
+    return tableService.getAllTableReservations();
+  }
 }
