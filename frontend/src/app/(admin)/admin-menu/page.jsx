b34afea6cb58b94151507b3314b67c76d74a97f8
KUMAR Parvesh (2022) WLIS205 <wlis205@live.rhul.ac.uk>
2024-03-23 23:12:50 +0000

    added dialog box for menu add form

--- frontend/src/app/(admin)/admin-menu/page.jsx
+++ frontend/src/app/(admin)/admin-menu/page.jsx
@@ -2,9 +2,22 @@
 
 import api from "@/app/services/api";
 import { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function MenuPage() {
   const [menus, setMenus] = useState([]);
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
 
   useEffect(() => {
     const fetchMenu = async () => {
@@ -20,13 +33,51 @@ export default function MenuPage() {
     fetchMenu();
   }, []);
 
+  console.log(menus);
+
   return (
     <div className="p-6">
       <div className="flex justify-between">
         <span className="text-3xl font-semibold">Menus ({menus.length})</span>
-        <button className="bg-green-800 text-white p-2 rounded-lg">
-          Add Menu
-        </button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <button className="bg-green-800 text-white px-2 rounded-lg">
+              Add Menu
+            </button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Add Menu</DialogTitle>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <label htmlFor="name" className="text-right">
+                  Name
+                </label>
+                <input
+                  id="name"
+                  defaultValue="Pedro Duarte"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <label htmlFor="username" className="text-right">
+                  Username
+                </label>
+                <input
+                  id="username"
+                  defaultValue="@peduarte"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <button className="bg-green-800 text-white px-2 rounded-lg">
+                Submit
+              </button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
       <div className="flex flex-col gap-3 mt-5">
         {menus.map((menu) => (
