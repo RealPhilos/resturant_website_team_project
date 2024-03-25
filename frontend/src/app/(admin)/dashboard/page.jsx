8562cf4202c5c881359c5211b71eb2641e8c0c1c
zlac157 <zlac157@live.rhul.ac.uk>
2024-03-25 17:28:59 +0000

    feat(dashboard): Added welcome page

--- frontend/src/app/(admin)/dashboard/page.jsx
+++ frontend/src/app/(admin)/dashboard/page.jsx
@@ -7,5 +7,31 @@ export default function DashboardPage() {
   const { user } = useContext(AuthContext);
 
   console.log(user);
-  return <div>DashboardPage</div>;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div className="bg-white shadow-lg hover:shadow-2xl rounded-lg p-8 m-4 w-full max-w-2xl transition duration-300 transform hover:scale-105 cursor-pointer">
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="text-2xl font-bold text-gray-700">Dashboard</h1>
+          <div className="text-sm font-semibold text-gray-500">
+            Welcome, {user?.username}
+          </div>
+        </div>
+        <div className="text-gray-700">
+          <p>This is your dashboard. From here you can:</p>
+          <ul className="list-disc list-inside">
+            <li>View and edit the menu </li>
+            <li>Manage the orders</li>
+          </ul>
+        </div>
+        <div className="flex justify-end">
+          {" "}
+          <a href="/" className="flex items-center">
+            <span className="text-gray-800">Return to </span>
+            <img src="/Icon.png" alt="Oaxaca Icon" className="w-12 h-12 ml-2" />
+            <span className="text-xl text-green-800 ml-2">axaca</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
