b455712ee1329d84d7791e6cc936d6f63027ca76
ahmedalyami <ahmadalyami2016@gmail.com>
2024-03-26 00:15:50 +0000

    added action buttons depending on the status

--- frontend/src/app/(admin)/admin-orders/order-list.jsx
+++ frontend/src/app/(admin)/admin-orders/order-list.jsx
@@ -30,6 +30,7 @@ export default function OrderList({ orders }) {
             Status
           </TableHead>
           <TableHead style={{ width: "150px" }}>Table</TableHead>
+          <TableHead>Action</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
@@ -45,6 +46,18 @@ export default function OrderList({ orders }) {
               {order.status}
             </TableCell>
             <TableCell>{order.tableNumber}</TableCell>
+            <TableCell>
+              {order.status == "COOKING" && (
+                <button className="text-white bg-blue-600 p-2 rounded-md">
+                  Mark as ready
+                </button>
+              )}
+              {order.status == "READY" && (
+                <button className="text-white bg-green-600 p-2 rounded-md">
+                  Mark as done
+                </button>
+              )}
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
