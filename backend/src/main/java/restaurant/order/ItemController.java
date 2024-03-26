28965e212710cde7b6a32b4bf046fc42229a55dd
ahmedalyami <ahmadalyami2016@gmail.com>
2024-03-26 00:37:08 +0000

    implemented updateOrderStatus function in the backend

--- backend/src/main/java/restaurant/order/ItemController.java
+++ backend/src/main/java/restaurant/order/ItemController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -87,6 +89,13 @@ public class ItemController {
     }
     itemService.addNewItem(item);
     return "Item added successfully";
+  }
+
+  @PutMapping(path = "{orderId}")
+  public void updateOrder(@PathVariable("orderId") Long orderId,
+      @RequestParam(required = false) String status) {
+    itemService.updateOrderStatus(orderId, status);
+
   }
 
   /**
