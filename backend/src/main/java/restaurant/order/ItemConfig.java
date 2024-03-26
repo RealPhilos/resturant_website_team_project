747d0fe109534a661d79e6418123013487bddeb3
zlac157 <zlac157@live.rhul.ac.uk>
2024-03-26 20:35:59 +0000

    Fix: Removed Unused Imports

--- backend/src/main/java/restaurant/order/ItemConfig.java
+++ backend/src/main/java/restaurant/order/ItemConfig.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
 
 /**
