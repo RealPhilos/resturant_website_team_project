cb7213caf7a46dd966769d7a1a9a92781ac70c7e
Zkac229 <zkac229@live.rhul.ac.uk>
2024-03-26 22:04:23 +0000

    feat(Menu): Can edit existing menu items

--- frontend/src/app/(admin)/admin-menu/page.jsx
+++ frontend/src/app/(admin)/admin-menu/page.jsx
@@ -15,6 +15,7 @@ import { useForm } from "react-hook-form";
 export default function MenuPage() {
   const [menus, setMenus] = useState([]);
 
+
   const {
     register,
     handleSubmit,
@@ -40,7 +41,6 @@ export default function MenuPage() {
     try {
       const newMenu = {
         ...data,
-        imgPaht: "",
       };
       const response = await api.post("/foods", newMenu);
       setMenus([...menus, newMenu]);
@@ -50,29 +50,175 @@ export default function MenuPage() {
     }
   };
 
+  const handleMenuEdit = async (data, id) => {
+    console.log(data, id);
+    try {
+      const updatedMenu = {
+        ...data,
+      };
+      const response = await api.put(`/foods/${id}`, updatedMenu);
+      if (response.status === 200) {
+        setMenus((prevMenus) =>
+          prevMenus.map((menu) => (menu.foodId === id ? updatedMenu : menu))
+        );
+        reset();
+      } else {
+        console.error("Failed to update menu:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error updating menu:", error);
+    }
+  };
+
+
+
   return (
     <div className="p-6">
-      <div className="flex justify-between">
-        <span className="text-3xl font-semibold">Menus ({menus.length})</span>
-        <Dialog>
+  <div className="flex justify-between">
+    <span className="text-3xl font-semibold">Menus ({menus.length})</span>
+    <Dialog onOpenChange={isOpen => {
+      console.log(isOpen);
+      if (isOpen) {
+        reset();
+      }
+}}>      <DialogTrigger asChild>
+        <button className="bg-green-800 text-white px-2 rounded-lg">
+          Add Menu
+        </button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Menu</DialogTitle>
+        </DialogHeader>
+        <form
+          className="flex flex-col gap-2"
+          onSubmit={handleSubmit(handleMenuAdd)}
+        >
+          <div className="flex flex-col items-start w-full gap-1">
+            <label htmlFor="name" className="text-right">
+              Name
+            </label>
+            <input
+              {...register("name", { required: true })}
+              className="pl-2 h-8 border border-gray-300 rounded-md w-full"
+            />
+            {errors.name && (
+              <span className="text-red-700">Menu name is required</span>
+            )}
+          </div>
+          <div className="flex flex-col items-start w-full gap-1">
+            <label htmlFor="imgPath" className="text-right">
+              ImgPath
+            </label>
+            <input
+              {...register("imgPath", { required: true })}
+              className="pl-2 h-8 border border-gray-300 rounded-md w-full"
+            />
+            {errors.price && (
+              <span className="text-red-700">Menu price is required</span>
+            )}
+          </div>
+          <div className="flex flex-col items-start w-full gap-1">
+            <label htmlFor="price" className="text-right">
+              Price
+            </label>
+            <input
+              type="number"
+              {...register("price", { required: true })}
+              className="pl-2 h-8 border border-gray-300 rounded-md w-full"
+            />
+            {errors.price && (
+              <span className="text-red-700">Menu price is required</span>
+            )}
+          </div>
+          <div className="flex flex-col items-start w-full gap-1">
+            <label htmlFor="category" className="text-right">
+              Category
+            </label>
+            <select
+              {...register("category", { required: true })}
+              className="pl-2 h-8 border border-gray-300 rounded-md w-full"
+            >
+              <option>Main</option>
+              <option>Sides</option>
+            </select>
+            {errors.category && (
+              <span className="text-red-700">
+                Menu category is required
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col items-start w-full gap-1">
+            <label htmlFor="description" className="text-right">
+              Description
+            </label>
+            <textarea
+              {...register("description", { required: true })}
+              className="pl-2 h-20 border border-gray-300 rounded-md w-full"
+            />
+            {errors.description && (
+              <span className="text-red-700 w-full">
+                Menu description is required
+              </span>
+            )}
+          </div>
+          <DialogClose className="">
+            <button className="w-full bg-green-800 text-white rounded-lg mt-4 py-3">
+              Submit
+            </button>
+          </DialogClose>
+        </form>
+      </DialogContent>
+    </Dialog>
+  </div>
+  <div className="flex flex-col gap-3 mt-5">
+  {menus.map((menu) => (
+    <div key={menu.foodId} className="border border-gray-400 rounded-lg flex justify-between">
+      <div className="flex gap-3">
+        <img
+          className="h-28 w-28 rounded-l-lg"
+          src={menu.imgPath}
+          alt=""
+        />
+        <div className="flex flex-col w-96 my-auto">
+          <span className="font-semibold text-xl">{menu.name}</span>
+          <span className="text-sm">{menu.description}</span>
+          <span className="text-xl text-gray-600 font-semibold">
+            £{menu.price}
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-col my-auto gap-3 mr-7">
+        <Dialog onOpenChange={isOpen => {
+      console.log(isOpen);
+      if (isOpen) {
+        reset();
+      }
+}}> 
           <DialogTrigger asChild>
             <button className="bg-green-800 text-white px-2 rounded-lg">
-              Add Menu
+              Edit
             </button>
           </DialogTrigger>
           <DialogContent className="sm:max-w-[425px]">
             <DialogHeader>
-              <DialogTitle>Add Menu</DialogTitle>
+              <DialogTitle>Edit</DialogTitle>
             </DialogHeader>
             <form
               className="flex flex-col gap-2"
-              onSubmit={handleSubmit(handleMenuAdd)}
+              onSubmit={(e) => {
+                e.preventDefault();
+                console.log(menu);
+                console.log(menu.foodId);
+                handleSubmit((data) => handleMenuEdit(data, menu.foodId))();
+              }}
             >
               <div className="flex flex-col items-start w-full gap-1">
                 <label htmlFor="name" className="text-right">
                   Name
                 </label>
                 <input
+                  defaultValue={menu.name} // Autofill with menu name
                   {...register("name", { required: true })}
                   className="pl-2 h-8 border border-gray-300 rounded-md w-full"
                 />
@@ -85,11 +231,12 @@ export default function MenuPage() {
                   ImgPath
                 </label>
                 <input
+                  defaultValue={menu.imgPath} // Autofill with menu image path
                   {...register("imgPath", { required: true })}
                   className="pl-2 h-8 border border-gray-300 rounded-md w-full"
                 />
-                {errors.price && (
-                  <span className="text-red-700">Menu price is required</span>
+                {errors.imgPath && (
+                  <span className="text-red-700">Menu image path is required</span>
                 )}
               </div>
               <div className="flex flex-col items-start w-full gap-1">
@@ -98,6 +245,7 @@ export default function MenuPage() {
                 </label>
                 <input
                   type="number"
+                  defaultValue={menu.price} // Autofill with menu price
                   {...register("price", { required: true })}
                   className="pl-2 h-8 border border-gray-300 rounded-md w-full"
                 />
@@ -110,6 +258,7 @@ export default function MenuPage() {
                   Category
                 </label>
                 <select
+                  defaultValue={menu.category} // Autofill with menu category
                   {...register("category", { required: true })}
                   className="pl-2 h-8 border border-gray-300 rounded-md w-full"
                 >
@@ -127,6 +276,7 @@ export default function MenuPage() {
                   Description
                 </label>
                 <textarea
+                  defaultValue={menu.description} // Autofill with menu description
                   {...register("description", { required: true })}
                   className="pl-2 h-20 border border-gray-300 rounded-md w-full"
                 />
@@ -144,36 +294,13 @@ export default function MenuPage() {
             </form>
           </DialogContent>
         </Dialog>
-      </div>
-      <div className="flex flex-col gap-3 mt-5">
-        {menus.map((menu) => (
-
-          <div className="border border-gray-400 rounded-lg flex justify-between">
-            <div className="flex gap-3">
-              <img
-                className="h-28 w-28 rounded-l-lg"
-                src={menu.imgPath}
-                alt=""
-              />
-              <div className="flex flex-col w-96 my-auto">
-                <span className="font-semibold text-xl">{menu.name}</span>
-                <span className="text-sm">{menu.description}</span>
-                <span className="text-xl text-gray-600 font-semibold">
-                  £{menu.price}
-                </span>
-              </div>
-            </div>
-            <div className="flex flex-col my-auto gap-3 mr-7">
-              <button className="bg-blue-600 text-white p-2 rounded-lg">
-                Edit
-              </button>
-              <button className="bg-red-600 text-white p-2 rounded-lg">
-                Delete
-              </button>
-            </div>
-          </div>
-        ))}
+        <button className="bg-red-600 text-white p-2 rounded-lg">
+          Delete
+        </button>
       </div>
     </div>
-  );
+  ))}
+    </div>
+  </div>
+);
 }
