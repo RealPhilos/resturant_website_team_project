1092fed54e03769207a04ef872b9c25f7fe542a3
KUMAR Parvesh (2022) WLIS205 <wlis205@live.rhul.ac.uk>
2024-03-26 16:44:40 +0000

    changed the data seeding for user

--- backend/src/main/java/restaurant/login/UserConfig.java
+++ backend/src/main/java/restaurant/login/UserConfig.java
@@ -20,8 +20,8 @@ public class UserConfig {
       User luque = new User("Luque", "123", "Chef");
       User malcolm = new User("Malcolm", "123", "Customer");
       User philip = new User("Philip", "123", "Chef");
-      User parvesh = new User("Parvesh", "123", "waiter");
-      User ahmed = new User("Ahmed", "123", "waiter");
+      User parvesh = new User("Parvesh", "123", "Waiter");
+      User ahmed = new User("Ahmed", "123", "Waiter");
       repository.saveAll(List.of(krish, luque, malcolm, philip, parvesh, ahmed));
     };
   }
